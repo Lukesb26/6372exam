00652404c2bd06254678c51bf7edb95ab082170a
Lukesb26 <lukebernfeld@gmail.com>
2014-11-10 20:34:01 -0600

    test sketch
    this is the sketch that i did for the test

--- Cross it, crack it, switch, update it/exam_switch_updateit/exam_switch_updateit.ino
+++ Cross it, crack it, switch, update it/exam_switch_updateit/exam_switch_updateit.ino
@@ -1,10 +1,23 @@
-
-
+const int button = A5;
+const int led1 = 5;
+const int led2 = 6;
+int buttonstate = 0;
 void setup() {
-  
+  pinMode (led1, OUTPUT);
+  pinMode (led2, OUTPUT);
+  pinMode (button, INPUT);
 }
 
 void loop(){
+  buttonstate = digitalRead (button);
+  if (buttonstate == HIGH) {
+    digitalWrite (led1, HIGH);
+    digitalWrite (led2, HIGH);
+  }
+  else { 
+    digitalWrite (led1, LOW);
+    digitalWrite (led2, LOW);
+  }
   
  
 }
